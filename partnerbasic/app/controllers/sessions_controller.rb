27f41b72eab7156ba3ed98d2c6fd430920216910
Alex6614 <alexma6614@gmail.com>
2018-04-19 02:11:46 -0400

    implemented sessions, and is_student helps distinguish between students and companies

--- partnerbasic/app/controllers/sessions_controller.rb
+++ partnerbasic/app/controllers/sessions_controller.rb
@@ -1,7 +1,21 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
+    @user = User.find_by(email: params[:email])
+    if @user.nil?
+      redirect_to login_path
+    elsif @user.password_hash == params[:password]
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
+    reset_session
+    redirect_to root_path
   end
 end
--- partnerbasic/config/routes.rb
+++ partnerbasic/config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
+  root 'welcome#index'
 
   get 'sessions/destroy'
 
   get 'session/create'
 
-  get 'session/destroy'
+  get 'sessions/new'
 
   resources :users
   resources :reviews
   resources :projects
-  root 'welcome#index'
 
   resources :applications
   resources :groups
   resources :clubs
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
